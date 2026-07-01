01247c0b4cdd418dad99021aba3e25cd80b98673
Md. Emran Hossain <emran@synesisit.info>
2026-07-01 11:28:33 +0600

    new features

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE IF NOT EXISTS finance_db_v2;
+USE finance_db_v2;
+
 CREATE TABLE IF NOT EXISTS users (
                                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                      username VARCHAR(50) UNIQUE NOT NULL,
